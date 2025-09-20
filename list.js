099c7361c513d0135dbffeedfd5ad0776182b67b
Yaroslav V. Derkach <Luxors@mail.com>
2025-09-20 22:20:24 +0300

    update

---
+++ list.js
@@ -0,0 +1,15 @@
+
+export const list = [
+  "github-finder",
+  "mogo",
+  "moneykeeper",
+  "new providence",
+  "react slider",
+  "tour of heroes",
+  "user crm",
+  "yoga",
+  "architect",
+  "flyu",
+  "shubox",
+  "wellm"
+]
---
+++ script.js
@@ -0,0 +1,29 @@
+"use strict";
+import { list } from './list.js';
+
+document.addEventListener('DOMContentLoaded', function() {
+  const listEl = document.getElementById('list');
+
+  if (!list?.length) {
+    listEl.innerHTML = '<li class="list-item">Проектов нет</li>';
+    return;
+  }
+
+  const fragment = document.createDocumentFragment();
+  
+  list.forEach((item) => {
+    const li = document.createElement('li');
+    li.className = 'list-item';
+    
+    const a = document.createElement('a');
+    a.href = `https://luxors.github.io/portfolio/${item}`;
+    a.target = '_blank';
+    a.className = 'list-item__link';
+    a.textContent = item;
+    
+    li.appendChild(a);
+    fragment.appendChild(li);
+  });
+  
+  listEl.appendChild(fragment);
+});
