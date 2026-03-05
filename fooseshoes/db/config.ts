8c8db1930e9097c3ba35e764a781fbf3ec372c5c
Yaroslav V. Derkach <Luxors@mail.com>
2026-03-05 22:51:20 +0300

    fooseshoes: add product to db

--- fooseshoes/db/config.ts
+++ fooseshoes/db/config.ts
@@ -1,6 +1,25 @@
-import { defineDb } from 'astro:db';
+import { defineDb, defineTable, column } from 'astro:db';
 
 // https://astro.build/db/config
+
+// Определяем таблицу для товаров интернет-магазина
+const Product = defineTable({
+  columns: {
+    productId: column.number({ primaryKey: true }),
+    title: column.text(),
+    description: column.text({ optional: true }),
+    price: column.number(),
+    imageUrl: column.text(),
+    isOnSale: column.boolean({ default: false }),
+    isAvailable: column.boolean({ default: true }),
+    isNewArrival: column.boolean({ default: false }),
+    isBestSeller: column.boolean({ default: false }),
+    isSpecialOffer: column.boolean({ default: false }),
+    createdAt: column.date({ default: new Date() }),
+    // categoryId: column.number({ references: () => Category.columns.id }), // так можно добавить связь с другой таблицей
+  },
+});
+
 export default defineDb({
-  tables: {}
+  tables: { Product },
 });
--- fooseshoes/db/seed.ts
+++ fooseshoes/db/seed.ts
@@ -1,6 +1,29 @@
-import { db } from 'astro:db';
+import { db, Product } from 'astro:db';
 
 // https://astro.build/db/seed
 export default async function seed() {
-	// TODO
+  await db.insert(Product).values([
+    {
+      productId: 1,
+      title: 'Even & Odd',
+      price: 149.99,
+      imageUrl: '/src/assets/images/newArrivals_item1.jpg',
+      isAvailable: true,
+    },
+    {
+      productId: 2,
+      title: 'Anna Field',
+      price: 3490,
+      imageUrl: '/src/assets/images/newArrivals_item2.jpg',
+      isAvailable: true,
+      isOnSale: true,
+    },
+    {
+      productId: 3,
+      title: 'Bullo Decollete',
+      price: 145.99,
+      imageUrl: '/src/assets/images/newArrivals_item3.jpg',
+      isAvailable: false,
+    },
+  ]);
 }
