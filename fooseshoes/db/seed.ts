5c8760d6dc79ec547ae1f3a786f4eca82956effe
Yaroslav V. Derkach <Luxors@mail.com>
2026-03-06 23:34:44 +0300

    fooseshoes: add products

--- fooseshoes/db/seed.ts
+++ fooseshoes/db/seed.ts
@@ -9,6 +9,7 @@ export default async function seed() {
       price: 149.99,
       imageUrl: '/src/assets/images/newArrivals_item1.jpg',
       isAvailable: true,
+      isNewArrival: true,
     },
     {
       productId: 2,
@@ -17,6 +18,7 @@ export default async function seed() {
       imageUrl: '/src/assets/images/newArrivals_item2.jpg',
       isAvailable: true,
       isOnSale: true,
+      isNewArrival: true,
     },
     {
       productId: 3,
@@ -24,6 +26,32 @@ export default async function seed() {
       price: 145.99,
       imageUrl: '/src/assets/images/newArrivals_item3.jpg',
       isAvailable: false,
+      isNewArrival: true,
+    },
+    {
+      productId: 4,
+      title: 'Even & Odd',
+      price: 149.99,
+      imageUrl: '/src/assets/images/bestSeller_item1.jpg',
+      isAvailable: true,
+      isBestSeller: true,
+    },
+    {
+      productId: 5,
+      title: 'Anna Field',
+      price: 3490,
+      imageUrl: '/src/assets/images/bestSeller_item2.jpg',
+      isAvailable: true,
+      isOnSale: true,
+      isBestSeller: true,
+    },
+    {
+      productId: 6,
+      title: 'Bullo Decollete',
+      price: 145.99,
+      imageUrl: '/src/assets/images/bestSeller_item3.jpg',
+      isAvailable: false,
+      isBestSeller: true,
     },
   ]);
 }
