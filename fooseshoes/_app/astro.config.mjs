ae2a19081d8976d812cfd5fc089c4f544b984b61
Yaroslav V. Derkach <Luxors@mail.com>
2026-03-23 22:06:14 +0300

    fooseshoes: deploy ++

---
+++ fooseshoes/_app/astro.config.mjs
@@ -0,0 +1,13 @@
+// @ts-check
+import { defineConfig } from 'astro/config';
+
+// https://astro.build/config
+export default defineConfig({
+  site: 'https://luxors.github.io',
+  base: '/portfolio/fooseshoes',
+  vite: {
+    css: {
+      transformer: 'lightningcss',
+    },
+  },
+});
