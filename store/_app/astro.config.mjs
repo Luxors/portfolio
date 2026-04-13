7d540d5f3a74698af94ce32cfb2659f11a2b3497
Yaroslav V. Derkach <Luxors@mail.com>
2026-04-13 11:56:57 +0300

    store: build-0.0.5

--- store/_app/astro.config.mjs
+++ store/_app/astro.config.mjs
@@ -4,6 +4,7 @@ import { defineConfig, passthroughImageService } from 'astro/config';
 // https://astro.build/config
 export default defineConfig({
   site: 'https://luxors.github.io',
+  base: '/portfolio/store',
   vite: {
     css: {
       transformer: 'lightningcss',
