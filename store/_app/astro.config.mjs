259dbd1714b1f293e3c2cffefb2533a5323eae03
Yaroslav V. Derkach <Luxors@mail.com>
2026-04-18 21:33:27 +0300

    store: build-0.0.5

--- store/_app/astro.config.mjs
+++ store/_app/astro.config.mjs
@@ -1,10 +1,17 @@
 // @ts-check
 import { defineConfig, passthroughImageService } from 'astro/config';
 
+import { loadEnv } from 'vite';
+
+// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+// @ts-expect-error
+// eslint-disable-next-line no-undef
+const { BASE_URL } = loadEnv(process.env.NODE_ENV, process.cwd(), '');
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://luxors.github.io',
-  base: '/portfolio/store',
+  base: BASE_URL,
   vite: {
     css: {
       transformer: 'lightningcss',
